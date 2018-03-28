f5a8aff8ea23106a41e1319f2bbc3f3db3643f25
baihuibo <baihuibo@hotmail.com>
2018-03-28 11:27:37 +0800

    修复了由于编码导致的中文乱码问题

--- create.sql
+++ create.sql
@@ -1,48 +1,76 @@
 CREATE TABLE product_info (
-	product_id VARCHAR (32) PRIMARY KEY ,
-	product_name VARCHAR (64) NOT NULL COMMENT '��Ʒ����',
-	product_price DECIMAL (8, 2) NOT NULL COMMENT '����',
-	product_stock INT NOT NULL COMMENT '���',
-	product_description VARCHAR (64) COMMENT '����',
-	product_icon VARCHAR (512) COMMENT 'Сͼ',
-	category_type INT NOT NULL COMMENT '��Ŀ����',
-	create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '����ʱ��',
-	update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '�޸�ʱ��'
-) COMMENT '��Ʒ��';
+  product_id          VARCHAR (32) PRIMARY KEY ,
+  product_name        VARCHAR(64)   NOT NULL
+  COMMENT '商品名称',
+  product_price       DECIMAL(8, 2) NOT NULL
+  COMMENT '单价',
+  product_stock       INT           NOT NULL
+  COMMENT '库存',
+  product_description VARCHAR(64) COMMENT '描述',
+  product_icon        VARCHAR(512) COMMENT '小图',
+  category_type       INT           NOT NULL
+  COMMENT '类目编码',
+  create_time         TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP
+  COMMENT '创建时间',
+  update_time         TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  COMMENT '修改时间'
+)
+  COMMENT '商品表';
 
 
 CREATE TABLE product_category (
-	categoryt_id INT NOT NULL PRIMARY KEY auto_increment,
-	category_name VARCHAR (64) NOT NULL COMMENT '��Ŀ����',
-	category_type INT NOT NULL UNIQUE COMMENT '��Ŀ����',
-	create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '����ʱ��',
-	update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '�޸�ʱ��'
-) COMMENT '��Ŀ��';
+  categoryt_id  INT         NOT NULL PRIMARY KEY auto_increment,
+  category_name VARCHAR(64) NOT NULL
+  COMMENT '类目名称',
+  category_type INT         NOT NULL UNIQUE
+  COMMENT '类目编码',
+  create_time   TIMESTAMP   NOT NULL             DEFAULT CURRENT_TIMESTAMP
+  COMMENT '创建时间',
+  update_time   TIMESTAMP   NOT NULL             DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  COMMENT '修改时间'
+)
+  COMMENT '类目表';
 
 CREATE TABLE order_master (
-	order_id VARCHAR (32) PRIMARY KEY,
-	buyer_name VARCHAR (32) NOT NULL COMMENT '��Ŀ����',
-	buyer_phone VARCHAR (32) NOT NULL COMMENT '�绰',
-	buyer_address VARCHAR (128) NOT NULL COMMENT '��ַ',
-	buyer_openid VARCHAR (64) NOT NULL COMMENT '΢��openid',
-	order_amount DECIMAL (8, 2) NOT NULL COMMENT '�������',
-	order_status TINYINT (3) NOT NULL COMMENT '����״̬��Ĭ��0���µ�',
-	pay_status TINYINT (3) NOT NULL COMMENT '֧��״̬��Ĭ��0δ֧��',
-	create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '����ʱ��',
-	update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '�޸�ʱ��',
+  order_id      VARCHAR (32) PRIMARY KEY,
+  buyer_name    VARCHAR(32)   NOT NULL
+  COMMENT '类目名称',
+  buyer_phone   VARCHAR(32)   NOT NULL
+  COMMENT '电话',
+  buyer_address VARCHAR(128)  NOT NULL
+  COMMENT '地址',
+  buyer_openid  VARCHAR(64)   NOT NULL
+  COMMENT '微信openid',
+  order_amount  DECIMAL(8, 2) NOT NULL
+  COMMENT '订单金额',
+  order_status  TINYINT(3)    NOT NULL
+  COMMENT '订单状态，默认0新下单',
+  pay_status    TINYINT(3)    NOT NULL
+  COMMENT '支付状态，默认0未支付',
+  create_time   TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP
+  COMMENT '创建时间',
+  update_time   TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  COMMENT '修改时间',
 	key idx_buyer_oper (buyer_openid)
-) COMMENT '������';
+)
+  COMMENT '订单表';
 
 CREATE TABLE order_detail (
-	detail_id VARCHAR (32) NOT NULL,
-	order_id VARCHAR (32) NOT NULL,
-	product_id VARCHAR (32) NOT NULL,
-	product_name VARCHAR (64) NOT NULL COMMENT '��Ʒ����',
-	product_price DECIMAL (8, 2) NOT NULL COMMENT '�۸�',
-	product_quantity INT NOT NULL COMMENT '����',
-	product_icon VARCHAR (512) COMMENT 'ͼ��',
-	create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '����ʱ��',
-	update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '�޸�ʱ��',
+  detail_id        VARCHAR (32)  NOT NULL,
+  order_id         VARCHAR (32)  NOT NULL,
+  product_id       VARCHAR (32)  NOT NULL,
+  product_name     VARCHAR(64)   NOT NULL
+  COMMENT '商品名称',
+  product_price    DECIMAL(8, 2) NOT NULL
+  COMMENT '价格',
+  product_quantity INT           NOT NULL
+  COMMENT '数量',
+  product_icon     VARCHAR(512) COMMENT '图标',
+  create_time      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP
+  COMMENT '创建时间',
+  update_time      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  COMMENT '修改时间',
 	PRIMARY KEY (detail_id),
 	KEY idx_order_id (order_id)
-) COMMENT '������';
+)
+  COMMENT '订单表';
